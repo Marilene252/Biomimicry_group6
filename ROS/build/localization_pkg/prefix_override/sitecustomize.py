15b53c01a2bc33aadbbec40d55ea43fed455189f
Marilene252 <marilene.oud@student.uva.nl>
2025-12-04 21:54:53 +0800

    ros

---
+++ ROS/build/localization_pkg/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/marilene/github/Biomimicry_group6/ROS/install/localization_pkg'
---
+++ ROS/build/localization_pkg/share/localization_pkg/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/marilene/github/Biomimicry_group6/ROS/build/localization_pkg"
